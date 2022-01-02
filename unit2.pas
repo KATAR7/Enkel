bee02816dc01da57713174978d62394dda53a928
KATAR7 <74004757+KATAR7@users.noreply.github.com>
2022-01-02 18:57:59 +0500

    Form2 files

---
+++ unit2.pas
@@ -0,0 +1,35 @@
+unit Unit2;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls,
+  Buttons;
+
+type
+
+  { TForm2 }
+
+  TForm2 = class(TForm)
+    BitBtn1: TBitBtn;
+    Label1: TLabel;
+    Panel1: TPanel;
+  private
+
+  public
+
+  end;
+
+var
+  Form2: TForm2;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm2 }
+
+end.
+
